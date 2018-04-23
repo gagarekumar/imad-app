66ec236f310580c220f7b59e60e3421e7eb4bd3f
gagarekumar <36849740+gagarekumar@users.noreply.github.com>
2018-04-23 09:49:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+var img = document.getElementById('madi')
+var marginLeft = 0;
+function moveRight (){
+    marginLeft = marginLeft + 10 ;
+    img.style.marginLeft = marginLeft + 'px';
+    
+}
+img.onClick = function ()
+{
+    var interval =setInterval(moveRight ,100);
+}
